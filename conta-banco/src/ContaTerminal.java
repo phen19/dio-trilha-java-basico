5f707101670dbd32e5442e13e49e6d0864eb5716
phen19 <phenriquelgsilva@gmail.com>
2024-05-16 19:53:27 -0300

    regras de negócio aplicadas

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         int numero;
@@ -5,7 +8,16 @@ public class ContaTerminal {
         String nomeCliente;
         double saldo;
 
-        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Por favor, digite o seu nome: ");
+        nomeCliente = scanner.next();
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = scanner.next();
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = scanner.nextInt();
+        System.out.println("Por favor, digite o saldo da conta: ");
+        saldo = scanner.nextDouble();
 
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo "+ saldo + " já está disponível para saque.");
     }
 }
