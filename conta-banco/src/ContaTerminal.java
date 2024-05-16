bf694d52a3556d0cca41a374576e47c8c0694432
phen19 <phenriquelgsilva@gmail.com>
2024-05-16 19:44:38 -0300

    meu projeto conta banco

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        
+
+    }
+}
---
+++ sistema-smart-tv/src/App.java
@@ -0,0 +1,31 @@
+public class App {
+    // public static void main(String[] args) throws Exception {
+    //     SmartTV smartTv = new SmartTV();
+
+    //     System.out.println("TV ligada? " + smartTv.ligada);
+    //     System.out.println("Canal Atual? " + smartTv.canal);
+    //     System.out.println("Volume atual? " + smartTv.volume);
+
+    //     smartTv.ligar();
+    //     System.out.println("TV Ligada? " + smartTv.ligada);
+
+    //     smartTv.diminuirVolume();
+    //     smartTv.diminuirVolume();
+    //     smartTv.diminuirVolume();
+    //     smartTv.aumentarVolume();
+    //     System.out.println("Volume? " + smartTv.volume);
+
+    //     System.out.println("Canal? " + smartTv.canal);
+    //     smartTv.mudarCanal(13);
+    //     System.out.println("Canal? " + smartTv.canal);
+    // }
+    public static void main(String[] args) throws Exception {
+
+        int numero1 = 1;
+  
+        String numero2 = "2";
+  
+        System.out.println(numero1+numero2); 
+  
+      }
+}
